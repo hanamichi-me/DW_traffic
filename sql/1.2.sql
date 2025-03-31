054ca935cf6db88181f8350fbe9457539ad63399
hanamichi-me <martinma0804@gmail.com>
2025-03-31 09:33:12 +0800

    恢复推送 output、DB_files_export、sources 文件夹

--- sql/1.2.sql
+++
@@ -1,8 +0,0 @@
--- 2. **哪些时间（月份/周几/时段）是致命事故高发期？**
-SELECT 
-    dd.month,
-    COUNT(*) AS death_count
-FROM fact_person_fatality fpf
-JOIN dim_date dd ON fpf.date_id = dd.date_id
-GROUP BY dd.month
-ORDER BY death_count DESC;
